7c996c3a2adac2fd8e34574bd1463ec03ec2fdaf
Rakesh G Sekhar <rakeshgsekhar.fortune@gmail.com>
2018-01-29 08:11:11 +0530

    Update MainActivity.java

--- AssigKMD/app/src/main/java/com/webatron/rakesh/assigkmd/MainActivity.java
+++ AssigKMD/app/src/main/java/com/webatron/rakesh/assigkmd/MainActivity.java
@@ -1,6 +1,8 @@
 package com.webatron.rakesh.assigkmd;
 
+import android.Manifest;
 import android.content.Intent;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
